e6fc0ffe9258b4f8c1cff5883036a6674ab2e5ad
Agni-ArwazJamal <arwaz@agnitechnologies.com>
2024-08-12 22:16:21 +0530

    [Add] api setup to test on dev server

---
+++ index.js
@@ -0,0 +1,144 @@
+// Load environment variables from .env file
+require('dotenv').config();
+const express = require('express');
+const OAuthClient = require('intuit-oauth');
+
+// Initialize an Express application
+const app = express();
+const port = 3000;
+const REALM_ID = process.env.REALM_ID;
+const MINOR_VERSION = process.env.MINOR_VERSION;
+// Configure the OAuthClient with credentials and environment
+const oauthClient = new OAuthClient({
+    clientId: process.env.CLIENT_ID, // QuickBooks OAuth2 Client ID
+    clientSecret: process.env.CLIENT_SECRET, // QuickBooks OAuth2 Client Secret
+    environment: process.env.ENVIRONMENT, // 'sandbox' or 'production'
+    redirectUri: process.env.REDIRECT_URL // Redirect URI for OAuth callbacks
+});
+
+// if(oauthClient.isAccessTokenValid()) {
+//     console.log("The access_token is valid");
+//  }
+ 
+const refreshToken = () => {
+    if(!oauthClient.isAccessTokenValid()){
+        oauthClient.refresh()
+            .then(function(authResponse) {
+                console.log('Tokens refreshed : ' + JSON.stringify(authResponse.json()));
+            })
+            .catch(function(e) {
+                console.error("The error message is :"+e.originalMessage);
+                console.error(e.intuit_tid);
+            });
+     
+     }
+}
+
+// Route to initiate the OAuth flow
+app.get('/auth', (req, res) => {
+    // Generate the authorization URL for QuickBooks
+    const authUri = oauthClient.authorizeUri({
+        scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
+        state: 'Init' // State to protect against CSRF attacks
+    });
+    // Redirect user to the QuickBooks authorization page
+    res.redirect(authUri);
+});
+
+// Callback route for handling the response from QuickBooks
+app.get('/callback', async (req, res) => {
+    const parseRedirect = req.url;
+
+    try {
+        // Create an OAuth token using the callback URL
+        const authResponse = await oauthClient.createToken(parseRedirect);
+        // Redirect to the payments route after successful authentication
+        console.log(authResponse);
+        res.redirect('/items');
+    } catch (e) {
+        console.error('Error', e);
+    }
+});
+
+// Route to fetch payments data from QuickBooks
+app.get('/items', async (req, res) => {
+    try {
+        // if(!oauthClient.isAccessTokenValid()){
+        //    await this.refreshToken();
+        // }
+        // Make an API call to QuickBooks to fetch payments
+        const selectQuery = 'select * from Item'
+        const response = await oauthClient.makeApiCall({
+            url: `https://sandbox-quickbooks.api.intuit.com/v3/company/${REALM_ID}/query?query=${selectQuery}&minorversion=${MINOR_VERSION}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        // Send the fetched data as JSON response
+        res.json(response.body);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Failed to fetch items');
+    }
+});
+
+// Route to fetch items based on Type and category from QuickBooks
+app.get('/items/filter', async (req, res) => {
+    const { category } = req.query;
+    const encodedQuery = encodeURIComponent(category);
+
+    if (!category) {
+        return res.status(400).send('category is required');
+    }
+
+    try {
+        // Make an API call to QuickBooks to fetch items based on Type and category
+        console.log(`https://sandbox-quickbooks.api.intuit.com//v3/company/9341452815412836/query?query=select * from Item where Type='${category}'&minorversion=73`);
+
+        const response = await oauthClient.makeApiCall({
+            url: `https://sandbox-quickbooks.api.intuit.com//v3/company/${REALM_ID}/query?query=select * from Item where Type='${category}'&minorversion=${MINOR_VERSION}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        // Send the fetched data as JSON response
+        res.json(response.body);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Failed to fetch items');
+    }
+});
+
+// Route to fetch a specific item by ID
+app.get('/items/:id', async (req, res) => {
+    const itemId = Number(req.params.id);
+    try {
+
+        if (isNaN(itemId)) {
+            return res.status(400).send('Invalid item ID');
+        }
+        // Make an API call to QuickBooks to fetch the item by ID
+        const response = await oauthClient.makeApiCall({
+            url: `https://sandbox-quickbooks.api.intuit.com/v3/company/${REALM_ID}/item/${itemId}?minorversion=${MINOR_VERSION}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        // Send the fetched item data as JSON response
+        console.log(response.body);
+        res.json(response.body);
+    } catch (e) {
+        console.error(e);
+        res.status(e.response.status).send(e.error);
+    }
+});
+
+
+
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
